407420d9d036cadfde336057c56e9d324217293f
David Schwarz <dsdavidschwarz@gmail.com>
2020-12-31 15:23:19 -0600

    wip new project

--- src/main.rs
+++ src/main.rs
@@ -2,30 +2,42 @@ use std::io;
 use std::io::Write;
 
 static UNKNOWN: &str = "unknown command, type help for more info";
+static NONAME: &str = "no name provided";
+static HELP: &str = "Arbor
+    A tree based dialogue editor
+
+    commands:
+    help - display this help menu
+    new - create a new project, node, or edge
+    project - get the current project info
+    node - get the current node info
+
+    exit application:
+    q 
+    quit 
+    exit";
+
+pub struct State {
+    project: ropey::Rope,
+    project_name: String,
+
+}
 
 mod cmd {
     use super::*;
     
-    pub fn help(cmd_iter: &mut std::str::Split<&str>) {
+    pub fn help(cmd_iter: &mut std::str::Split<&str>,
+                state: &mut State, 
+    ) {
         let _ = cmd_iter;
-        println!(
-            "
-            Arbor
-            A tree based dialogue editor
-            
-            commands:
-            help - display this help menu
-
-            q 
-            quit 
-            exit 
-            "
-        );
+        println!("{}", HELP);
     }
 
-    pub fn new(cmd_iter: &mut std::str::Split<&str>) {
+    pub fn new(cmd_iter: &mut std::str::Split<&str>,
+               state: &mut State,
+    ) {
         match cmd_iter.next() {
-            Some("project") => new::project(cmd_iter),
+            Some("project") => new::project(cmd_iter, state),
             Some("node") => new::node(cmd_iter),
             Some("edge") => new::edge(cmd_iter),
             _ => println!("{}", UNKNOWN)
@@ -33,9 +45,29 @@ mod cmd {
     }
 
     mod new {
-        pub fn project(cmd_iter: &mut std::str::Split<&str>) {
-            let _test = ropey::Rope::from_str("a;falkj;jgdflkkljdlsfkjlkslakjslaksjflsdjlkfjlaksjlaskfjak");
+        use super::*;
 
+        pub fn project(cmd_iter: &mut std::str::Split<&str>, state: &mut State) {
+            // create a new text file.
+            let project_name = cmd_iter.next();
+            match project_name {
+                None => {println!("{}", NONAME); return}
+                _ =>{}
+            }
+            // TODO: use openoptions to not overwrite
+            let project_file = std::fs::File::create(project_name.unwrap()).unwrap();
+            // drop project to save file
+            drop(project_file);
+            // Open text file as a rope
+            let mut project = ropey::Rope::from_reader(
+                std::io::BufReader::new(
+                    std::fs::File::open(project_name.unwrap()).unwrap()
+                )
+            ).unwrap();
+
+            // save info to state
+            state.project_name = project_name.unwrap().to_string();
+            state.project = project;
         }
 
         pub fn node(cmd_iter: &mut std::str::Split<&str>) {
@@ -52,6 +84,12 @@ fn main() {
     let mut buf = String::with_capacity(100);    
     println!("welcome to arbor!");
 
+    // TODO: clean up state init
+    let mut state = State {
+        project: ropey::Rope::new(),
+        project_name: String::new(),
+    };
+
     loop {
         print!(">> ");
         io::stdout().flush().unwrap();
@@ -64,8 +102,8 @@ fn main() {
             .split(" "); 
 
         match cmd_iter.next().unwrap() {
-            "help" => cmd::help(&mut cmd_iter),
-            "new" => cmd::new(&mut cmd_iter),
+            "help" => cmd::help(&mut cmd_iter, &mut state),
+            "new" => cmd::new(&mut cmd_iter, &mut state),
             "q" => break,
             "exit" => break, 
             "quit" => break,
