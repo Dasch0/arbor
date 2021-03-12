9f6035ea5159a87ae664a1d8f5ef5adec36a3e36
David Schwarz <dsdavidschwarz@gmail.com>
2021-03-11 22:53:02 -0600

    add table and new key command

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use derive_new::*;
 use enum_dispatch::*;
 use enum_from_str::ParseEnumVariantError;
 use enum_from_str_derive::FromStr;
+use hashbrown::HashMap;
 use petgraph::prelude::*;
 use petgraph::visit::IntoNodeReferences;
 use petgraph::*;
@@ -14,7 +15,7 @@ use structopt::*;
 use crate::cmd::Executable;
 
 // TODO: Major Features
-// 1. enums for edge function calls
+// 1. Actionable edge function calls, currently impossible to do anything with action::Kind enum
 // 2. Name list & name validation
 // 3. Node and edge validation
 // 4. Tests
@@ -27,6 +28,29 @@ static TREE_EXT: &str = ".tree";
 static _NONAME: &str = "no name provided";
 static _UNIMPLEMENTED: &str = "unimplemented command";
 static SUCCESS: &str = "success\r\n";
+
+/// Top level data structure for storing a dialogue tree
+///
+/// This struct contains the tree representing the dialogue nodes and player actions connecting
+/// them, the rope which stores all text in a tightly packed manner, and a hashtable for storing
+/// variables such as player names, conditionals, etc.
+#[derive(new, Serialize, Deserialize)]
+pub struct DialogueTreeData {
+    tree: petgraph::graph::DiGraph<Section, Choice>,
+    rope: SerialRope,
+    table: HashMap<String, String>,
+    name: String,
+}
+impl DialogueTreeData {
+    fn default() -> Self {
+        DialogueTreeData {
+            tree: graph::DiGraph::<Section, Choice>::with_capacity(512, 2048),
+            rope: SerialRope::new(ropey::Rope::new()),
+            table: HashMap::new(),
+            name: String::new(),
+        }
+    }
+}
 
 /// typedef representing a section of text in a rope. This section contains a start and end index,
 /// stored in an array. The first element should always be smaller than the second
@@ -88,21 +112,6 @@ impl<'de> Deserialize<'de> for SerialRope {
     }
 }
 
-#[derive(new, Serialize, Deserialize)]
-pub struct EditorState {
-    tree: petgraph::graph::DiGraph<Section, Choice>,
-    rope: SerialRope,
-    name: String,
-}
-impl EditorState {
-    fn default() -> Self {
-        EditorState {
-            tree: graph::DiGraph::<Section, Choice>::with_capacity(512, 2048),
-            rope: SerialRope::new(ropey::Rope::new()),
-            name: String::new(),
-        }
-    }
-}
 
 mod cmd {
     use super::*;
@@ -113,7 +122,7 @@ mod cmd {
     /// Trait to allow structopt generated
     #[enum_dispatch]
     pub trait Executable {
-        fn execute(&self, state: &mut EditorState) -> cmd::Result;
+        fn execute(&self, data: &mut DialogueTreeData) -> cmd::Result;
     }
 
     /// A tree based dialogue editor
@@ -142,6 +151,7 @@ mod cmd {
             Project(new::Project),
             Node(new::Node),
             Edge(new::Edge),
+            Key(new::Key),
         }
 
         /// Create a new project
@@ -164,17 +174,18 @@ mod cmd {
         impl Executable for Project {
             // Create a new file on disk for new project, optionally set it as active in the editor
             // state
-            fn execute(&self, state: &mut EditorState) -> cmd::Result {
-                let mut new_state = EditorState::new(
+            fn execute(&self, data: &mut DialogueTreeData) -> cmd::Result {
+                let mut new_state = DialogueTreeData::new(
                     graph::DiGraph::<Section, Choice>::with_capacity(512, 2048),
                     SerialRope::new(ropey::Rope::new()),
+                    HashMap::new(),
                     self.name.clone(),
                 );
 
                 cmd::Save::new().execute(&mut new_state)?;
 
                 if self.set_active {
-                    *state = new_state;
+                    *data = new_state;
                     Ok("New project created and set as active")
                 } else {
                     Ok("New project created on disk")
@@ -196,14 +207,14 @@ mod cmd {
         impl Executable for Node {
             /// Create a new section of text on the text rope, and then make a new node on the
             /// tree pointing to the section
-            fn execute(&self, state: &mut EditorState) -> cmd::Result {
-                let start = state.rope.rope.len_chars();
-                state.rope.rope.append(ropey::Rope::from(format!(
+            fn execute(&self, data: &mut DialogueTreeData) -> cmd::Result {
+                let start = data.rope.rope.len_chars();
+                data.rope.rope.append(ropey::Rope::from(format!(
                     "{}::{}",
                     self.speaker, self.dialogue
                 )));
-                let end = state.rope.rope.len_chars();
-                state.tree.add_node([start, end]);
+                let end = data.rope.rope.len_chars();
+                data.tree.add_node([start, end]);
                 Ok(SUCCESS)
             }
         }
@@ -229,11 +240,11 @@ mod cmd {
         }
 
         impl Executable for Edge {
-            fn execute(&self, state: &mut EditorState) -> cmd::Result {
-                let start = state.rope.rope.len_chars();
-                state.rope.rope.append(ropey::Rope::from(self.text.clone()));
-                let end = state.rope.rope.len_chars();
-                state.tree.add_edge(
+            fn execute(&self, data: &mut DialogueTreeData) -> cmd::Result {
+                let start = data.rope.rope.len_chars();
+                data.rope.rope.append(ropey::Rope::from(self.text.clone()));
+                let end = data.rope.rope.len_chars();
+                data.tree.add_edge(
                     NodeIndex::from(self.start_index),
                     NodeIndex::from(self.end_index),
                     Choice::new([start, end], self.action.unwrap_or_default()),
@@ -241,6 +252,31 @@ mod cmd {
                 Ok(SUCCESS)
             }
         }
+
+        /// Create a new key in the dialogue tree
+        ///
+        /// A key represents some variable  text a segment of dialogue from a character.
+        #[derive(new, StructOpt, Debug)]
+        #[structopt(setting = AppSettings::NoBinaryName)]
+        pub struct Key {
+            /// The keyword to reference the value with in the text 
+            key: String,
+            /// Value to store, able be updated by player actions
+            value: String,
+        }
+        impl Executable for Key {
+            fn execute(&self, data: &mut DialogueTreeData) -> cmd::Result {
+                // Check that the key doesn't already exist, since we want new to not overwrite
+                // values. The user can use edit commands for that
+                if data.table.get(&self.key).is_some() {
+                    Ok("Key already exists")
+                } else {
+                    data.table.insert(self.key.clone(), self.value.clone());
+                    Ok(SUCCESS)
+                }
+            }
+        }
+
     }
 
     mod edit {
@@ -271,25 +307,25 @@ mod cmd {
         impl Executable for Node {
             /// Create a new section of text on the text rope, and then make a new node on the
             /// tree pointing to the section
-            fn execute(&self, state: &mut EditorState) -> cmd::Result {
+            fn execute(&self, data: &mut DialogueTreeData) -> cmd::Result {
                 let node_index = NodeIndex::new(self.node_id);
-                let node = state
+                let node = data
                     .tree
                     .node_weight_mut(node_index)
                     .ok_or_else(cmd::Error::default)?;
-                let old_weight = *node;
+                let old_weight: Section = *node;
 
-                let start = state.rope.rope.len_chars();
-                state.rope.rope.append(ropey::Rope::from(format!(
+                let start = data.rope.rope.len_chars();
+                data.rope.rope.append(ropey::Rope::from(format!(
                     "{}::{}",
                     self.speaker, self.dialogue
                 )));
-                let end = state.rope.rope.len_chars();
+                let end = data.rope.rope.len_chars();
 
                 *node = [start, end];
 
-                util::prune(old_weight, &mut state.rope.rope, &mut state.tree);
-                state.tree.add_node([start, end]);
+                util::prune(old_weight, &mut data.rope.rope, &mut data.tree);
+                data.tree.add_node([start, end]);
                 Ok(SUCCESS)
             }
         }
@@ -319,17 +355,17 @@ mod cmd {
         }
 
         impl Executable for Edge {
-            fn execute(&self, state: &mut EditorState) -> cmd::Result {
+            fn execute(&self, data: &mut DialogueTreeData) -> cmd::Result {
                 let edge_index = EdgeIndex::<u32>::new(self.edge_id);
-                let edge = state
+                let edge = data
                     .tree
                     .edge_weight_mut(edge_index)
                     .ok_or_else(cmd::Error::default)?;
                 let old_weight = *edge;
 
-                let start = state.rope.rope.len_chars();
-                state.rope.rope.append(ropey::Rope::from(self.text.clone()));
-                let end = state.rope.rope.len_chars();
+                let start = data.rope.rope.len_chars();
+                data.rope.rope.append(ropey::Rope::from(self.text.clone()));
+                let end = data.rope.rope.len_chars();
                 let new_weight = Choice::new([start, end], self.action.unwrap_or_default());
 
                 // Handle deletion/recreation of edge if nodes need to change
@@ -340,11 +376,11 @@ mod cmd {
                     let target_node_index =
                         NodeIndex::new(self.target_node_id.ok_or_else(cmd::Error::default)?);
 
-                    state.tree.remove_edge(edge_index);
-                    state
+                    data.tree.remove_edge(edge_index);
+                    data
                         .tree
                         .add_edge(source_node_index, target_node_index, new_weight);
-                    util::prune(old_weight.text, &mut state.rope.rope, &mut state.tree);
+                    util::prune(old_weight.text, &mut data.rope.rope, &mut data.tree);
                 }
 
                 Ok(SUCCESS)
@@ -358,9 +394,9 @@ mod cmd {
     pub struct Save {}
 
     impl Executable for Save {
-        fn execute(&self, state: &mut EditorState) -> cmd::Result {
-            let json = serde_json::to_string(&state).unwrap();
-            std::fs::write(state.name.clone() + TREE_EXT, json)?;
+        fn execute(&self, data: &mut DialogueTreeData) -> cmd::Result {
+            let json = serde_json::to_string(&data).unwrap();
+            std::fs::write(data.name.clone() + TREE_EXT, json)?;
             Ok(SUCCESS)
         }
     }
@@ -373,11 +409,11 @@ mod cmd {
     }
 
     impl Executable for Load {
-        fn execute(&self, state: &mut EditorState) -> cmd::Result {
-            let new_state: EditorState = serde_json::from_reader(std::io::BufReader::new(
+        fn execute(&self, data: &mut DialogueTreeData) -> cmd::Result {
+            let new_data: DialogueTreeData = serde_json::from_reader(std::io::BufReader::new(
                 std::fs::File::open(self.name.clone() + TREE_EXT)?,
             ))?;
-            *state = new_state;
+            *data = new_data;
             Ok(SUCCESS)
         }
     }
@@ -393,12 +429,12 @@ mod cmd {
     pub struct List {}
 
     impl Executable for List {
-        fn execute(&self, state: &mut EditorState) -> cmd::Result {
-            let node_iter = state.tree.node_references();
+        fn execute(&self, data: &mut DialogueTreeData) -> cmd::Result {
+            let node_iter = data.tree.node_references();
             node_iter.for_each(|n| {
                 // Print node identifier, node text, and then all edges
-                println!("{:#?} : {}", n.0, state.rope.rope.slice(n.1[0]..n.1[1]));
-                state
+                println!("{:#?} : {}", n.0, data.rope.rope.slice(n.1[0]..n.1[1]));
+                data
                     .tree
                     .edges_directed(n.0, petgraph::Direction::Outgoing)
                     .for_each(|e| {
@@ -406,7 +442,7 @@ mod cmd {
                             "--> {:#?} : {} : {} ",
                             e.target(),
                             e.id().index(),
-                            state
+                            data
                                 .rope
                                 .rope
                                 .slice(e.weight().text[0]..e.weight().text[1])
@@ -424,17 +460,17 @@ mod cmd {
     pub struct Read {}
 
     impl Executable for Read {
-        fn execute(&self, state: &mut EditorState) -> cmd::Result {
+        fn execute(&self, data: &mut DialogueTreeData) -> cmd::Result {
             println!("reader mode:");
             let node_idx = graph::node_index(0);
-            let iter = state
+            let iter = data
                 .tree
                 .edges_directed(node_idx, petgraph::Direction::Outgoing);
             let _target_list = iter.enumerate().map(|(i, e)| {
                 println!(
                     "{}. {}",
                     i,
-                    state
+                    data
                         .rope
                         .rope
                         .slice(e.weight().text[0]..e.weight().text[1])
@@ -546,9 +582,7 @@ mod action {
     pub enum Kind {
         /// No action
         Inactive,
-        /// Stores a specific word or phrase to the Hashtable with a provided key, if the key is
-        /// already used, the value is updated. If the key is new, a new entry in the table is
-        /// created
+        /// Stores a specific word or phrase to the Hashtable with a provided key
         Store,
         /// Similar function to store, except the word or phrase is provided by the user
         StorePrompt,
@@ -565,7 +599,7 @@ fn main() {
     let mut cmd_buf = String::with_capacity(1000);
 
     // TODO: clean up state init
-    let mut state = EditorState::default();
+    let mut state = DialogueTreeData::default();
     loop {
         // print default header
         println!("------------");
