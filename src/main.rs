d5988b8d01c2ce271e4da9c5238a1ae8aacdfb84
David Schwarz <dsdavidschwarz@gmail.com>
2021-03-15 00:17:31 -0500

    WIP tests and benchmarks

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![feature(test)]
+extern crate test;
 use crate::cmd::Executable;
 use clap::AppSettings;
 use derive_new::*;
@@ -788,5 +790,70 @@ fn main() {
 
         // clear input buffers before starting next input loop
         cmd_buf.clear();
+    }
+}
+
+
+#[cfg(test)]
+mod bench {
+    use super::*;
+    use test::Bencher;
+
+    #[bench]
+    /// Benchmark node parsing worst case, many substitutions and improperly sized buffer
+    fn stress_parse_node(b: &mut Bencher) {
+        let mut name_table = HashMap::<String, String>::new();
+        name_table.insert("Elle".to_string(), "Amberson".to_string());
+        name_table.insert("Patrick".to_string(), "Breakforest".to_string());
+        name_table.insert("Anna".to_string(), "Catmire".to_string());
+        name_table.insert("Laura".to_string(), "Dagson".to_string());
+        name_table.insert("John".to_string(), "Elliot".to_string());
+
+        let text = "Elle::xzunz::Anna::lxn ::Elle::cn::Patrick::o::Laura::sokxt::Patrick::eowln
+        ::Patrick::::John::c::Patrick::iw qyyhr.jxhccpyvchze::Anna::ox hi::Laura::nlv::John::peh
+        swvnismjs::John::p::Laura::::John::slu.hlqzkei jhrskiswe::John::::John::m.rx::Patrick::pk
+        te::Elle::h::John::m,z,.jwtol::Elle::h rwvnpuqw::John::::John::::Elle::tnz::Elle::.kv.
+        ::Laura::xyxml jrsei::John::jlsl nysn::Patrick::mvvu.up::Laura::jh,t,,jnwheu npnxqcowev
+        ::Anna::,::Elle::.emiv::John::ezoqy::Elle::cppyxtos,miqphi::Elle::.q c::Patrick::nzms
+        skno::Laura:: mkzn.::Patrick::x::John::s jhl::John::ow::John::nj hsk::Elle::ihwpens rx
+        ::Patrick::nn..iurtxcou::Laura::hypkqoyqyz.iihu::Elle::umcpvl::Patrick::::Anna::.cjh,cn
+        phey::Patrick::hxorixcyr::Anna::u::Anna::  heuneszejtwrkewiymmq::John::ynjvh::Laura::lvvtunm
+        ::Laura::i.rk::Patrick::hk::Elle::knvmml::John::j::Anna::::Anna::pslllnmtcyjzesls moj ttm
+        ::Elle::jrr,mh,::Anna:: pyl::Anna::owunpjve::John::::Laura:: ::Anna::xci::Patrick::p::Laura::
+        l.iwn::Elle::lnjx::Laura::oyo::Anna::eq,n::Elle::ej.::Laura::hh";
+
+        b.iter(|| {
+            let mut name_buf = String::with_capacity(1);
+            let mut buf = String::with_capacity(1);
+            let res = cmd::util::parse_node(text, &name_table, &mut name_buf, &mut buf);
+            test::black_box(&res);
+            test::black_box(&name_buf);
+            test::black_box(&buf);
+        });
+    }
+
+    #[bench]
+    /// Benchmark standard node parsing case, few substitutions and pre-allocated buffer
+    fn quick_parse_node(b: &mut Bencher) {
+        let mut name_table = HashMap::<String, String>::new();
+        name_table.insert("vamp".to_string(), "Dracula".to_string());
+        name_table.insert("king".to_string(), "King Laugh".to_string());
+
+        let text = "It is a strange world, a sad world, a world full of miseries, and woes, and troubles.
+        And yet when King Laugh come, he make them all dance to the tune he play. Bleeding hearts, 
+        and dry bones of the churchyard, and tears that burn as they fall, all dance together to the music
+        that he make with that smileless mouth of him. Ah, we men and women are like ropes drawn tight with
+        strain that pull us different ways. Then tears come, and like the rain on the ropes, they brace us 
+        up, until perhaps the strain become too great, and we break. But King Laugh he come like the
+        sunshine, and he ease off the strain again, and we bear to go on with our labor, what it may be.";
+
+        let mut name_buf = String::with_capacity(20);
+        let mut buf = String::with_capacity(text.len() + 50);
+        b.iter(|| {
+            let res = cmd::util::parse_node(text, &name_table, &mut name_buf, &mut buf);
+            test::black_box(&res);
+            test::black_box(&name_buf);
+            test::black_box(&buf);
+        });
     }
 }
