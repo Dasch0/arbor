e12a2640dfbd28699187cb1bf912a81106aa3b08
David Schwarz <dsdavidschwarz@gmail.com>
2022-03-19 21:44:23 -0500

    passes miri

--- yarn-parser/src/lib.rs
+++ yarn-parser/src/lib.rs
@@ -1,7 +1,32 @@
 #![doc = include_str!("../README.md")]
 pub mod pool;
 
-/// scan through a loaded .yarn file, finding variables and instantiating them (with initial values) into a hashmap
+// TODO(david): 
+//  * SIMD parsing
+//
+/// scan through a .yarn file loaded into memory, finding variables and instantiating them 
+/// (with initial values) into a hashmap
+#[inline(always)]
+pub fn find_variables(yarn: &str) {
+    let mut state = false;
+    let mut start_idx = 0; // tracks start of a found variable
+    for (idx, ch) in yarn.chars().enumerate() {
+        if state {
+            match ch {
+                'A'..='Z' | 'a'..='z' | '0'..='9' => {}, // allowed variable name characters
+                _ => {
 
-pub fn find_variables(_var: &mut usize) {
+                }, // variable name captured
+            };
+            state = false;
+        } else {
+            if ch == '$' {
+                start_idx = idx;
+                state = false;
+            }
+        }
+    }
 }
+
+pub fn generate_graph() {}
+
